99b5cdd9e9aa571a45e1cf61ec59caf68617af3c
Haruka Kido <58123962+harukakido@users.noreply.github.com>
2023-03-05 09:20:18 -0600

    Add files via upload

---
+++ Lab2_Part2_SerialMonitor.ino
@@ -0,0 +1,13 @@
+String incoming;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0)
+  {
+    incoming = Serial.readString();
+    Serial.println(incoming);
+  }
+}
