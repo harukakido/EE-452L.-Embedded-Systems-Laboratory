dc47c1d4492101117f68bb8ef03bf01e92f59a38
Haruka Kido <58123962+harukakido@users.noreply.github.com>
2023-04-06 09:20:58 -0500

    Add files via upload

---
+++ Lab5_7SegmentDisplay.ino
@@ -0,0 +1,259 @@
+const int a = 12;
+const int b = 10;
+const int c = 8;
+const int d = 7;
+const int e = 6;
+const int f = 11;
+const int g = 9;
+int p = 0;
+
+int startStopReset = 13; 
+
+const int d1 = 1;
+const int d2 = 2;
+const int d3 = 3;
+const int d4 = 4;
+
+long n = 1000000; //start time
+int x = 100; 
+int del = 75; //delay
+ 
+void setup()
+{
+  pinMode(d1, OUTPUT);
+  pinMode(d2, OUTPUT);
+  pinMode(d3, OUTPUT);
+  pinMode(d4, OUTPUT);
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(p, OUTPUT);
+  pinMode(startStopReset, INPUT); 
+  digitalWrite(startStopReset, HIGH); 
+}
+ 
+void loop()
+{
+  digitalWrite (p, HIGH);
+
+  clearLEDs();
+  pickDigit(1);
+  pickNumber((n/x/1000)%10);
+  delayMicroseconds(del);
+ 
+  clearLEDs();
+  pickDigit(2);
+  pickNumber((n/x/100)%10);
+  delayMicroseconds(del);
+ 
+  clearLEDs();
+  pickDigit(3);
+  dispDec(3);
+  pickNumber((n/x/10)%10);
+  delayMicroseconds(del);
+ 
+  clearLEDs();
+  pickDigit(4);
+  pickNumber(n/x%10);
+  delayMicroseconds(del);
+ 
+  n--; //'n++' for stopwatch
+ 
+  if (digitalRead(13) == LOW)
+  {
+    n = 1000000; //re-start time
+  }
+}
+ 
+void pickDigit(int x)
+{
+  digitalWrite(d1, HIGH);
+  digitalWrite(d2, HIGH);
+  digitalWrite(d3, HIGH);
+  digitalWrite(d4, HIGH);
+ 
+  switch(x)
+  {
+  case 1: 
+    digitalWrite(d1, LOW); 
+    break;
+  case 2: 
+    digitalWrite(d2, LOW); 
+    break;
+  case 3: 
+    digitalWrite(d3, LOW);
+    digitalWrite(p, HIGH); 
+    break;
+  default: 
+    digitalWrite(d4, LOW); 
+    break;
+  }
+}
+ 
+void pickNumber(int x)
+{
+  switch(x)
+  {
+  default: 
+    zero(); 
+    break;
+  case 1: 
+    one(); 
+    break;
+  case 2: 
+    two(); 
+    break;
+  case 3: 
+    three(); 
+    break;
+  case 4: 
+    four(); 
+    break;
+  case 5: 
+    five(); 
+    break;
+  case 6: 
+    six(); 
+    break;
+  case 7: 
+    seven(); 
+    break;
+  case 8: 
+    eight(); 
+    break;
+  case 9: 
+    nine(); 
+    break;
+  }
+}
+ 
+void dispDec(int x)
+{
+  digitalWrite(p, LOW);
+}
+ 
+void clearLEDs()
+{
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+  digitalWrite(p, LOW);
+}
+ 
+void zero()
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, LOW);
+}
+ 
+void one()
+{
+  digitalWrite(a, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+}
+ 
+void two()
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, LOW);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, LOW);
+  digitalWrite(g, HIGH);
+}
+ 
+void three()
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, HIGH);
+}
+ 
+void four()
+{
+  digitalWrite(a, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+ 
+void five()
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, LOW);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+ 
+void six()
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, LOW);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+ 
+void seven()
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+}
+ 
+void eight()
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+ 
+void nine()
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
